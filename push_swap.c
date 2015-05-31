d94de08eef01e0ec016a8e1036f467a2a52bbb5b
Sulaiman YUSOF <syusof@e3r6p2.42.fr>
2015-05-31 19:58:23 +0200

    r

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,14 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/18 17:22:30 by syusof            #+#    #+#             */
-/*   Updated: 2015/05/31 19:49:18 by syusof           ###   ########.fr       */
+/*   Updated: 2015/05/31 19:57:05 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+
+#include <stdio.h>
 
 int		main(int arc,char **arv)
 {
@@ -28,5 +31,6 @@ int		main(int arc,char **arv)
 		lst_add(&e, create_lst(a));
 		arc--;
 	}
+	printf("%d",((t_numb*)e->content)->val);
 	return (0);
 }
