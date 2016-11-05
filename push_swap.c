59fd8c7058cb44610ac487838b8ce04bfd3102a5
Sulaiman YUSOF <syusof@e3r7p4.42.fr>
2016-11-05 15:19:18 +0100

    r

--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,13 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/18 17:22:30 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/05 15:16:24 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/05 15:18:52 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "push_swap.h"
 
-int		main(int ac,char **av)
+int		main(int ac, char **av)
 {
 	t_lst	**lsta;
 
@@ -24,14 +25,14 @@ int		main(int ac,char **av)
 	}
 	while (ac >= 2)
 	{
-		if(ft_checkint(av[ac - 1]) == 0)
+		if (ft_checkint(av[ac - 1]) == 0)
 		{
 			write(2, "Error\n", 6);
 			return (0);
 		}
 		else
 		{
-			if(ft_p1(&lsta,ac,av) == 0)
+			if (ft_p1(&lsta, ac, av) == 0)
 				return (0);
 		}
 		ac--;
