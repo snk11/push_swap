d0f6c46d61a5233b4a6e71c7afdf6d29013fbdbf
Sulaiman YUSOF <syusof@e3r7p4.42.fr>
2016-11-05 15:01:20 +0100

    r

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/18 17:22:30 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/05 14:15:48 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/05 15:00:41 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,23 +58,6 @@ int		main(int ac,char **av)
 		}
 		ac--;
 	}
-//	ft_sort(&lsta);
-	
 	ft_quicksort(&lsta);
-//	ft_freelst(lsta);
-//	ft_p1(&lst1);
-	//lsta = sort_list(lsta,croissant);
-
-//	ft_printlst(*lsta);
-//	while (lsta)
-//	{
-//		printf("%d\n",((t_numb*)lsta->content)->val);
-//		lsta = lsta->next;
-//	}
-/*
-	while(42)
-	{
-	}
-	*/
 	return (0);
 }
