6ae89233e71972c0428332503cafc5445fc2e296
Sulaiman YUSOF <syusof@e3r12p17.42.fr>
2016-11-17 19:31:53 +0100

    r

--- ft_checkint.c
+++ ft_checkint.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/14 11:53:11 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/15 15:50:00 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/17 19:31:39 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,112 @@ int		ft_checkint(char *s)
 	s1 = s;
 	if(*s1 == '-' || *s1 == '+')
 	{
+		if (ft_checkint_p1(s, s1) == 0)
+			return (0);
+	}
+
+
+
+
+
+
+
+
+
+
+
+	else if (ft_strlen_isdigit(s) == 10)
+	{
+
+				if(*s1 == '2')
+				{
+					s1++;
+					if(*s1 == '1')
+					{
+						s1++;
+						if(*s1 == '4')
+						{
+							s1++;
+							if(*s1 == '7')
+							{
+								s1++;
+								if(*s1 == '4')
+								{
+									s1++;
+									if(*s1 == '8')
+									{
+										s1++;
+										if(*s1 == '3')
+										{
+											s1++;
+											if(*s1 == '6')
+											{
+												s1++;
+												if(*s1 == '4')
+												{
+													s1++;
+													if(*s1 == '7')
+													{
+													}
+													else if(*s1 > '7')
+														return (0);
+												}
+												else if(*s1 > '4')
+													return (0);
+											}
+											else if(*s1 > '6')
+												return (0);
+										}
+										else if(*s1 > '3')
+											return (0);
+									}
+									else if(*s1 > '8')
+										return (0);
+								}
+								else if(*s1 > '4')
+									return (0);
+							}
+							else if(*s1 > '7')
+								return (0);
+						}
+						else if(*s1 > '4')
+							return (0);
+					}
+					else if(*s1 > '1')
+						return (0);
+				}
+				else if (*s1 > '2')
+					return (0);
+	}
+	else if (ft_strlen_isdigit(s) > 10)
+		return (0);
+	/*
+	while(*s1)
+	{
+		if (!(*s1 >= '0' && *s1 <= '9'))
+		{
+			if(*s1 != ' ' && *s1 != '\t')
+				return (0);
+			while(*s1 && (*s1 == ' ' || *s1 == '\t'))
+					s1++;
+			if(*s1)
+				return (0);
+			else
+				return (1);
+		}
+		s1++;
+	}
+	*/
+	return (1);
+}
+
+
+
+int		ft_checkint_p1(char *s, char *s1)
+{
 		if (ft_strlen_isdigit(s) == 11)
 		{
+
 			if(*s1 == '-')
 			{
 				s1++;
@@ -162,88 +266,5 @@ int		ft_checkint(char *s)
 			return (0);
 		else
 			s1++;
-	}
-	else if (ft_strlen_isdigit(s) == 10)
-	{
-
-				if(*s1 == '2')
-				{
-					s1++;
-					if(*s1 == '1')
-					{
-						s1++;
-						if(*s1 == '4')
-						{
-							s1++;
-							if(*s1 == '7')
-							{
-								s1++;
-								if(*s1 == '4')
-								{
-									s1++;
-									if(*s1 == '8')
-									{
-										s1++;
-										if(*s1 == '3')
-										{
-											s1++;
-											if(*s1 == '6')
-											{
-												s1++;
-												if(*s1 == '4')
-												{
-													s1++;
-													if(*s1 == '7')
-													{
-													}
-													else if(*s1 > '7')
-														return (0);
-												}
-												else if(*s1 > '4')
-													return (0);
-											}
-											else if(*s1 > '6')
-												return (0);
-										}
-										else if(*s1 > '3')
-											return (0);
-									}
-									else if(*s1 > '8')
-										return (0);
-								}
-								else if(*s1 > '4')
-									return (0);
-							}
-							else if(*s1 > '7')
-								return (0);
-						}
-						else if(*s1 > '4')
-							return (0);
-					}
-					else if(*s1 > '1')
-						return (0);
-				}
-				else if (*s1 > '2')
-					return (0);
-	}
-	else if (ft_strlen_isdigit(s) > 10)
-		return (0);
-	/*
-	while(*s1)
-	{
-		if (!(*s1 >= '0' && *s1 <= '9'))
-		{
-			if(*s1 != ' ' && *s1 != '\t')
-				return (0);
-			while(*s1 && (*s1 == ' ' || *s1 == '\t'))
-					s1++;
-			if(*s1)
-				return (0);
-			else
-				return (1);
-		}
-		s1++;
-	}
-	*/
 	return (1);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/31 17:57:24 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/17 18:00:56 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/17 18:49:56 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -191,5 +191,6 @@ int		ft_parse1_p2_p1(t_lst *****lsta, int i, char *s1, t_numb *e);
 int		ft_parse1_p2_p2(t_lst *****lsta, int i, char *s1, t_numb *e);
 void		ft_parse1_p2_p3(char **s1);
 int			ft_parse1_p3(char **s, int *ind1);
+int		ft_checkint_p1(char *s, char *s1);
 
 #endif
