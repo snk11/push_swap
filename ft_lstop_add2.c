5895a7fdff5795f3625dbfecb40fa88098a30a56
Sulaiman YUSOF <syusof@e3r6p4.42.fr>
2016-11-18 15:42:32 +0100

    r

--- ft_lstop_add2.c
+++ ft_lstop_add2.c
@@ -1,75 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstop_add.c                                     :+:      :+:    :+:   */
+/*   ft_lstop_add2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/09 19:22:42 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/18 15:40:11 by syusof           ###   ########.fr       */
+/*   Created: 2016/11/18 15:39:25 by syusof            #+#    #+#             */
+/*   Updated: 2016/11/18 15:40:06 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void		ft_lstop_add1(t_lst *****lstop, int p)
-{
-	t_numb	*e;
-	t_lst	*lstmp;
-
-	e = NULL;
-	e = (t_numb*)malloc(sizeof(t_numb));
-	e->val = p;
-	lstmp = create_lst(e);
-	free(e);
-	e = NULL;
-	lst_add13(&lstop, lstmp);
-}
-
-void		ft_lstop_add2(t_lst ****lstop, int p)
-{
-	t_numb	*e;
-	t_lst	*lstmp;
-
-	e = NULL;
-	e = (t_numb*)malloc(sizeof(t_numb));
-	e->val = p;
-	lstmp = create_lst(e);
-	free(e);
-	e = NULL;
-	lst_add14(&lstop, lstmp);
-}
-
-void		ft_lstop_add3(t_lst ******lstop, int p)
-{
-	t_numb	*e;
-	t_lst	*lstmp;
-
-	e = NULL;
-	e = (t_numb*)malloc(sizeof(t_numb));
-	e->val = p;
-	lstmp = create_lst(e);
-	free(e);
-	e = NULL;
-	lst_add15(&lstop, lstmp);
-}
-
-void		ft_lstop_add_down0(t_lst ***toplist, int p)
+void		ft_lstop_add_down2(t_lst ****toplist, int p)
 {
 	t_numb	*e;
 	t_lst	*lstmp;
 	t_lst	*lstmp2;
 
 	lstmp = NULL;
-	lstmp2 = **toplist;
+	lstmp2 = ***toplist;
 	e = NULL;
 	e = (t_numb*)malloc(sizeof(t_numb));
 	e->val = p;
 	lstmp = create_lst(e);
 	free(e);
 	e = NULL;
-	if (**toplist == NULL)
-		**toplist = lstmp;
+	if (***toplist == NULL)
+		***toplist = lstmp;
 	else
 	{
 		while (lstmp2 && lstmp2->next)
@@ -78,22 +36,46 @@ void		ft_lstop_add_down0(t_lst ***toplist, int p)
 	}
 }
 
-void		ft_lstop_add_down1(t_lst *****toplist, int p)
+void		ft_lstop_add_down3(t_lst ******toplist, int p)
 {
 	t_numb	*e;
 	t_lst	*lstmp;
 	t_lst	*lstmp2;
 
 	lstmp = NULL;
-	lstmp2 = ****toplist;
+	lstmp2 = *****toplist;
 	e = NULL;
 	e = (t_numb*)malloc(sizeof(t_numb));
 	e->val = p;
 	lstmp = create_lst(e);
 	free(e);
 	e = NULL;
-	if (****toplist == NULL)
-		****toplist = lstmp;
+	if (*****toplist == NULL)
+		*****toplist = lstmp;
+	else
+	{
+		while (lstmp2 && lstmp2->next)
+			lstmp2 = lstmp2->next;
+		lstmp2->next = lstmp;
+	}
+}
+
+void		ft_lstop_add_down4(t_lst *******toplist, int p)
+{
+	t_numb	*e;
+	t_lst	*lstmp;
+	t_lst	*lstmp2;
+
+	lstmp = NULL;
+	lstmp2 = ******toplist;
+	e = NULL;
+	e = (t_numb*)malloc(sizeof(t_numb));
+	e->val = p;
+	lstmp = create_lst(e);
+	free(e);
+	e = NULL;
+	if (******toplist == NULL)
+		******toplist = lstmp;
 	else
 	{
 		while (lstmp2 && lstmp2->next)
