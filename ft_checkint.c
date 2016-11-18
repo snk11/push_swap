23d96e684bc2dbbcda87a7de9eee383abdab193b
Sulaiman YUSOF <syusof@e3r6p4.42.fr>
2016-11-18 19:35:00 +0100

    r

--- ft_checkint.c
+++ ft_checkint.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/14 11:53:11 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/18 15:20:30 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/18 19:33:53 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,18 @@ int		ft_checkint(char *s)
 		return (0);
 	while (*s && (*s == ' ' || *s == '\t'))
 		s++;
-	s1 = s;
-	if (*s1 == '-' || *s1 == '+')
-		return (ft_checkint_p1(s, s1));
-	else if (ft_strlen_isdigit(s) == 10)
-		return (ft_checkint_p2(s, s1));
-	else if (ft_strlen_isdigit(s) > 10)
-		return (0);
-	return (1);
+	if(*s)
+	{
+		s1 = s;
+		if (*s1 == '-' || *s1 == '+')
+			return (ft_checkint_p1(s, s1));
+		else if (ft_strlen_isdigit(s) == 10)
+			return (ft_checkint_p2(s, s1));
+		else if (ft_strlen_isdigit(s) > 10)
+			return (0);
+		return (1);
+	}
+	return (0);
 }
 
 int		ft_checkint_p1(char *s, char *s1)
