03fffb246c7e57306eecf8d0cc835ec5e28c2127
Sulaiman YUSOF <syusof@e3r6p4.42.fr>
2016-11-18 15:04:49 +0100

    r

--- ft_checkdouble.c
+++ ft_checkdouble.c
@@ -6,17 +6,17 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/14 16:37:40 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/18 13:25:26 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/18 15:03:07 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int		ft_checkdouble(t_lst *lsta,int d)
+int		ft_checkdouble(t_lst *lsta, int d)
 {
-	while(lsta)
+	while (lsta)
 	{
-		if(((t_numb*)(lsta->content))->val == d)
+		if (((t_numb*)(lsta->content))->val == d)
 			return (0);
 		lsta = lsta->next;
 	}
