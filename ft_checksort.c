b0685573da40b52bcaded4e6ae4d1611c48a2c2a
Sulaiman YUSOF <syusof@e3r6p4.42.fr>
2016-11-18 15:25:40 +0100

    r

--- ft_checksort.c
+++ ft_checksort.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 16:40:42 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/14 15:37:33 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/18 15:25:27 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,19 @@ int		ft_checksort(t_lst *lstb)
 
 	lst1 = ft_copylst(lstb);
 	i = 0;
-	while(i < (ft_comptelem(lst1)) - 1)
+	while (i < (ft_comptelem(lst1)) - 1)
 	{
-		if(ft_comptelem(lst1) >= 2)
+		if (ft_comptelem(lst1) >= 2)
 		{
-			if(((t_numb*)(lst1)->content)->val > ((t_numb*)((lst1)->next)->content)->val)
+			if (((t_numb*)(lst1)->content)->val
+					> ((t_numb*)((lst1)->next)->content)->val)
 			{
 				swap(&lst1);
 				ft_freelst(&lst1);
 				return (0);
 			}
 		}
-		if(ft_comptelem((lst1)) >= 2)
+		if (ft_comptelem((lst1)) >= 2)
 		{
 			rotate(&lst1);
 		}
