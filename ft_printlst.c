99c2c58f00a4ef52374e160dc651f4b6d4d3a461
Sulaiman YUSOF <syusof@e3r6p4.42.fr>
2016-11-18 16:22:25 +0100

    r

--- ft_printlst.c
+++ ft_printlst.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 12:15:46 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/18 13:48:04 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/18 16:22:05 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 void	ft_printlst(t_lst *lst1)
 {
-	while(lst1)
+	while (lst1)
 	{
-		if(((t_numb*)(lst1)->content)->val == 1)
-			write(1,"sa\n",3);
-		if(((t_numb*)(lst1)->content)->val == 2)
-			write(1,"ra\n",3);
-		if(((t_numb*)(lst1)->content)->val == 3)
-			write(1,"rra\n", 4);
-		if(((t_numb*)(lst1)->content)->val == 4)
-			write(1,"pa\n",3);
-		if(((t_numb*)(lst1)->content)->val == 22)
-			write(1,"rb\n",3);
-		if(((t_numb*)(lst1)->content)->val == 23)
-			write(1,"rrb\n", 4);
-		if(((t_numb*)(lst1)->content)->val == 24)
-			write(1,"pb\n",3);
+		if (((t_numb*)(lst1)->content)->val == 1)
+			write(1, "sa\n", 3);
+		if (((t_numb*)(lst1)->content)->val == 2)
+			write(1, "ra\n", 3);
+		if (((t_numb*)(lst1)->content)->val == 3)
+			write(1, "rra\n", 4);
+		if (((t_numb*)(lst1)->content)->val == 4)
+			write(1, "pa\n", 3);
+		if (((t_numb*)(lst1)->content)->val == 22)
+			write(1, "rb\n", 3);
+		if (((t_numb*)(lst1)->content)->val == 23)
+			write(1, "rrb\n", 4);
+		if (((t_numb*)(lst1)->content)->val == 24)
+			write(1, "pb\n", 3);
 		lst1 = lst1->next;
 	}
 }
