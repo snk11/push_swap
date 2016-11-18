6630364ce4846da2f6033faf6e6309fae59c9014
Sulaiman YUSOF <syusof@e3r6p4.42.fr>
2016-11-18 15:02:28 +0100

    r

--- ft_checkdigit.c
+++ ft_checkdigit.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 15:25:17 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/18 13:25:19 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/18 15:02:14 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ int		ft_checkdigit(char *s)
 	int		i;
 
 	i = 0;
-	while(s[i])
+	while (s[i])
 	{
-		if(s[i] == '-' || s[i] == '+')
+		if (s[i] == '-' || s[i] == '+')
 		{
 			if (!(s[i + 1]))
 				return (0);
-			if ((i != 0 && (s[i-1] != ' ' && s[i - 1] != '\t'))
-					|| (s[i + 1] && !ft_isdigit(s[i+1])))
+			if ((i != 0 && (s[i - 1] != ' ' && s[i - 1] != '\t'))
+					|| (s[i + 1] && !ft_isdigit(s[i + 1])))
 				return (0);
 		}
 		else if (!ft_isdigit(s[i]) && s[i] != ' ' && s[i] != '\t')
-				return (0);
-		i++;;
+			return (0);
+		i++;
 	}
 	return (1);
 }
