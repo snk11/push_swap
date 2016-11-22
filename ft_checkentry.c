b89afb75a55216cd2c469b1e28ed2374cdb1c1ce
Sulaiman YUSOF <syusof@e1r6p20.42.fr>
2016-11-22 16:24:20 +0100

    r

--- ft_checkentry.c
+++ ft_checkentry.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 15:12:57 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/22 16:07:46 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/22 16:24:04 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int		ft_checkentry(t_option *option)
 		{
 			if (ft_checkint(option->av[ac - 1]) == 0)
 			{
-				write(2, "Error\n", 6);
 				write(2, "Error\n", 6);
 				if ((fd = open("error_file", O_CREAT | O_WRONLY,
 								S_IRUSR | S_IWUSR)))
